9f5bb682246d6154f6f6a49a88d5cea52cbf8601
Andrii Balitskyi <10balian10@gmail.com>
2024-04-04 16:03:33 +0200

    Fix init seam test

--- test/test_init_seam.py
+++ test/test_init_seam.py
@@ -6,4 +6,4 @@ def test_init_seam_with_fixture(seam: Seam):
     assert seam.api_url
     assert seam.lts_version
     assert "http" in seam.api_url
-    assert seam.wait_for_action_attempt == False
+    assert seam.wait_for_action_attempt is False
