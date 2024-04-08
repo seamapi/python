a1db82dc0e5a3cc65dfca94ef506c220f341cabb
Andrii Balitskyi <10balian10@gmail.com>
2024-04-08 19:51:14 +0200

    Update generate script to pass openapi spec from @seamapi/types

---
+++ scripts/generate.js
@@ -0,0 +1,30 @@
+import { generatePythonSDK, writeFs } from '@seamapi/nextlove-sdk-generator'
+import { openapi } from '@seamapi/types/connect'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const SEAM_DIRECTORY_PREFIX = 'seam/'
+const OUTPUT_DIRECTORY = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  '../',
+)
+
+const main = async () => {
+  try {
+    const pythonSdkFileSystem = await generatePythonSDK({
+      openApiSpecObject: openapi,
+    })
+
+    const seamFiles = Object.entries(pythonSdkFileSystem).filter(([fileName]) =>
+      fileName.startsWith(SEAM_DIRECTORY_PREFIX),
+    )
+
+    writeFs(OUTPUT_DIRECTORY, Object.fromEntries(seamFiles))
+
+    console.log('Python SDK generated successfully!')
+  } catch (error) {
+    console.error('Failed to generate SDK:', error)
+  }
+}
+
+await main().catch((error) => console.error('Unexpected error:', error))
