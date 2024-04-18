0f104058b7c7d6d0ed07f2178ac4091fc61712bb
Andrii Balitskyi <10balian10@gmail.com>
2024-04-18 16:53:42 +0200

    Fix workspace test

--- test/workspaces/test_workspaces.py
+++ test/workspaces/test_workspaces.py
@@ -8,5 +8,5 @@ def test_workspaces(seam: Seam):
     ws_list = seam.workspaces.list()
     assert len(ws_list) > 0
 
-    reset_sandbox_result = seam.workspaces.reset_sandbox()
-    assert reset_sandbox_result is None
+    reset_sandbox_action_attempt = seam.workspaces.reset_sandbox()
+    assert reset_sandbox_action_attempt.action_type == 'RESET_SANDBOX_WORKSPACE'
