1721803b36e522fe70fa5f2aa02d751735cf4216
Evan Sosenko <evan@getseam.com>
2024-04-18 21:52:38 -0700

    Simplify generate

---
+++ generate-routes.js
@@ -0,0 +1,26 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import {
+  generatePythonSDK as generateSdk,
+  writeFs,
+} from '@seamapi/nextlove-sdk-generator'
+import { openapi } from '@seamapi/types/connect'
+import { deleteAsync } from 'del'
+
+const libName = 'seam'
+
+const rootPath = dirname(fileURLToPath(import.meta.url))
+const outputPath = resolve(rootPath, libName)
+
+await deleteAsync(outputPath)
+
+const fileSystem = await generateSdk({
+  openApiSpecObject: openapi,
+})
+
+const files = Object.entries(fileSystem).filter(([fileName]) =>
+  fileName.startsWith(`${libName}/`),
+)
+
+writeFs(rootPath, Object.fromEntries(files))
