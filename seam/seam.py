8ba79cfccfbb02571f183e01ba00d45183984d2c
Andrii Balitskyi <10balian10@gmail.com>
2024-05-23 16:04:16 +0200

    Remove client and client_options

--- seam/seam.py
+++ seam/seam.py
@@ -26,8 +26,6 @@ class Seam(AbstractSeam):
         workspace_id: Optional[str] = None,
         endpoint: Optional[str] = None,
         wait_for_action_attempt: Optional[Union[bool, Dict[str, float]]] = False,
-        client: Optional[requests.Session] = None,
-        client_options: Optional[Dict[str, Any]] = None,
     ):
         """
         Parameters
@@ -42,10 +40,6 @@ class Seam(AbstractSeam):
           The API endpoint to which the request should be sent.
         wait_for_action_attempt : bool or dict, optional
           Controls whether to wait for an action attempt to complete, either as a boolean or as a dictionary specifying `timeout` and `poll_interval`. Defaults to `False`.
-        client : requests.Session, optional
-          A pre-configured requests session to be used for making HTTP requests. If not provided, a new `SeamHttpClient` instance will be created using the `client_options` and other relevant parameters.
-        client_options : dict, optional
-          A dictionary of options that will be passed to the `SeamHttpClient` constructor when initializing a new requests session client. This allows for customization of the HTTP client, such as setting additional headers or configuring timeouts. For detailed information on available options, refer to the niquests library [repo](https://github.com/jawah/niquests). If client is provided, this parameter will be ignored.
         """
 
         self.lts_version = Seam.lts_version
