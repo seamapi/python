6ffa7ccf045a1b92daf50b359d94c0f929ab0e05
Andrii Balitskyi <10balian10@gmail.com>
2024-05-30 19:48:13 +0200

    Run the fake-seam-connect server entrypoint directly without an additional script

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,16 @@
+import socket
 import pytest
 import subprocess
 import time
 import os
 from seam import Seam
 from contextlib import contextmanager
+
+
+def find_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 0))
+        return s.getsockname()[1]
 
 
 # Create a custom context manager to ensure the subprocess is terminated correctly
@@ -22,24 +29,18 @@ def subprocess_popen(*args, **kwargs):
 
 @pytest.fixture(scope="function")
 def fake_seam_server():
-    script_path = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "start-fake-seam-server.js")
-    )
+    port = find_free_port()
+    os.environ["PORT"] = str(port)
 
     with subprocess_popen(
-        ["node", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
-    ) as process:
+        ["npm", "run", "start:fake-seam-connect"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ):
         # Allow some time for the server to start
         time.sleep(0.5)
 
-        # Read the first line of stdout for the endpoint
-        endpoint = process.stdout.readline().strip()
-
-        if not endpoint:
-            stderr = process.stderr.read()
-            raise RuntimeError(
-                f"Failed to start the fake-seam-connect server: {stderr}"
-            )
+        endpoint = f"http://localhost:{port}"
 
         yield endpoint
 
