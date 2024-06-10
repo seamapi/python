972ca35e087c3c23501e4589b0d10f99ad9ee98c
Andrii Balitskyi <84702959+andrii-balitskyi@users.noreply.github.com>
2024-06-10 15:50:10 +0200

    Use fake-seam-connect module to run the tests (#67)
    * Use fake-seam-connect module to run the tests
    
    * Start fake server for every test fn separately
    
    * Fix lint
    
    * Setup node during test workflow
    
    * Use npm registry for fake-seam-connect
    
    * Use context manager to fix lint
    
    * Run the fake-seam-connect server entrypoint directly without an additional script
    
    * Remove start-fake-seam-server.js
    
    * Don't pipe subprocess's output and error + minor corrections
    
    * Update fake server start script name, pin fake-seam-connect version
    
    * Rename find_free_port to get_port
    
    * ci: Generate code
    
    * Update command that starts fake server in conftest.py
    
    ---------
    
    Co-authored-by: Seam Bot <devops@getseam.com>

--- test/conftest.py
+++ test/conftest.py
@@ -1,13 +1,47 @@
+import socket
 import pytest
+import subprocess
+import time
+import os
 from seam import Seam
-import random
-import string
+from contextlib import contextmanager
+
+
+def get_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("", 0))
+        return s.getsockname()[1]
+
+
+# Create a custom context manager to ensure the fake server subprocess is terminated correctly
+@contextmanager
+def subprocess_popen(*args):
+    process = subprocess.Popen(*args)
+    try:
+        yield process
+    finally:
+        process.terminate()
+        try:
+            process.wait(timeout=10)
+        except subprocess.TimeoutExpired:
+            process.kill()
 
 
 @pytest.fixture(scope="function")
-def seam():
-    r = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
-    seam = Seam(
-        endpoint=f"https://{r}.fakeseamconnect.seam.vc", api_key="seam_apikey1_token"
-    )
+def fake_seam_connect_server():
+    port = get_port()
+    os.environ["PORT"] = str(port)
+
+    with subprocess_popen(["npm", "run", "start"]):
+        # Allow some time for the server to start
+        time.sleep(0.5)
+
+        endpoint = f"http://localhost:{port}"
+
+        yield endpoint
+
+
+@pytest.fixture(scope="function")
+def seam(fake_seam_connect_server):
+    seam = Seam(endpoint=fake_seam_connect_server, api_key="seam_apikey1_token")
     yield seam
