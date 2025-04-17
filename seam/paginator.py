61bf8b21455b02641bceee86f03a9f721e4363ea
Andrii Balitskyi <10balian10@gmail.com>
2025-04-17 14:44:49 +0200

    Move pagination class into a separate file

--- seam/paginator.py
+++ seam/paginator.py
@@ -1,18 +1,7 @@
 from typing import Callable, Dict, Any, Tuple, Generator, List
 from .client import SeamHttpClient
 from niquests import Response, JSONDecodeError
-
-
-class Pagination:
-    def __init__(
-        self,
-        has_next_page: bool,
-        next_page_cursor: str | None,
-        next_page_url: str | None,
-    ):
-        self.has_next_page = has_next_page
-        self.next_page_cursor = next_page_cursor
-        self.next_page_url = next_page_url
+from .pagination import Pagination
 
 
 class Paginator:
